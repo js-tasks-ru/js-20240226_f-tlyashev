37b3ad11dd088013caa1b1f1570cee27d1edf27b
Tliashev.FF <f.tlyashev@gmail.com>
2024-03-01 16:25:52 +0500

    add solution for trim-symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,28 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+  if (string.length === 0 || size <= 0) {
+    return "";
+  } else if (!size) {
+    return string;
+  }
 
+  let result = "";
+  let counter = 1;
+  let prevChar = string[0];
+
+  for (let i = 1; i < string.length; i++) {
+    if (string[i] === prevChar) {
+      counter++;
+    } else {
+      counter = 1;
+      prevChar = string[i];
+    }
+
+    if (counter <= size) {
+      result += string[i];
+    }
+  }
+
+  return string[0] + result;
 }
