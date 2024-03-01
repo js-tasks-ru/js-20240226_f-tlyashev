9ed81a2b5cf714cfe1ff8800769f73717a161e78
Tliashev.FF <f.tlyashev@gmail.com>
2024-03-01 23:47:31 +0500

    add solution for create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const keys = path.split(".");
+  return function (obj) {
+    let value = obj;
+    for (let key of keys) {
+      if (value[key] === undefined) {
+        return undefined;
+      }
+      value = value[key];
+    }
+    return value;
+  };
 }
