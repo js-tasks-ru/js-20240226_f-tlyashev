bf8f908f97fbdf295198338b96475ace5bfabeba
Tliashev.FF <f.tlyashev@gmail.com>
2024-03-12 16:02:27 +0500

    add solution 05-01-notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,58 @@
 export default class NotificationMessage {
+  static lastMessage;
+  element;
 
+  constructor(message = "", { duration = 0, type = "" } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.element = this.createElement(this.createTemplate());
+  }
+
+  createElement(template) {
+    const element = document.createElement("div");
+
+    element.innerHTML = template;
+
+    return element.firstElementChild;
+  }
+
+  createTemplate() {
+    return `
+        <div class="notification ${this.type}" 
+            style="--value:${this.duration / 1000}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+                <div class="notification-header">${this.type}</div>
+                <div class="notification-body">
+                    ${this.message}
+                </div>
+            </div>
+        </div>
+      `;
+  }
+
+  show(container = document.body) {
+    if (NotificationMessage.lastMessage) {
+      NotificationMessage.lastMessage.remove();
+    }
+
+    NotificationMessage.lastMessage = this;
+
+    this.timerId = setTimeout(() => {
+      this.remove();
+    }, this.duration);
+
+    container.appendChild(this.element);
+  }
+
+  remove() {
+    this.element.remove();
+    clearTimeout(this.timerId);
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
